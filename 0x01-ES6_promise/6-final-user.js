5f758a1b04c6cce63946f18ec54b553223be65db
Menya96 <menyahtonny@gmail.com>
2024-10-03 11:26:27 +0000

    error fix in 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const [signUpResult, uploadResult] = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
-  });
+  ]);
+
+  return [
+    { status: signUpResult.status, value: signUpResult.value || signUpResult.reason },
+    { status: uploadResult.status, value: uploadResult.value || uploadResult.reason },
+  ];
 }
